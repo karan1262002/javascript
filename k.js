fa91065615c3a25158ed61f1cd97dbc9eff63b4a
Karan M <123549481+karan1262002@users.noreply.github.com>
2023-10-01 21:04:12 +0530

    Add files via upload

---
+++ k.js
@@ -0,0 +1,102 @@
+const videos=[
+    {
+        id: 1,
+        title: "Mind Voice Sothaanigal",
+        thumbnailUrl: "https://www.youtube.com/embed/m2U_VBM4XjU",
+        likes: 480000,
+        views: 1500000,
+        channelName: "Micset",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKbubOe2HHYK_R2cBMwwU18WFe--zWUDtgWXWIQ4-w=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+    
+    {
+        id: 2,
+        title: "🥰 A Wonderful Day in ZOHO 🥳 i Just Loved it 😍",
+        thumbnailUrl: "https://www.youtube.com/embed/6KvcChyHHoM",
+        likes: 27000,
+        views: 905507,
+        channelName: "Tech Boss",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKb35986qBgEO4JGhWQuBmrP43YC76tS8tvQXzPy=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+
+    {
+        id: 3,
+        title: "Jawan : Hayyoda (Tamil)",
+        thumbnailUrl: "https://www.youtube.com/embed/8eYG5QGZAZs" 
+        likes: 313000,
+        views: 2200000,
+        channelName: "T-Series Tamil",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKajCyM1CVqEMJjKmEppUx3eBMHxiZQhxmPXZt2eLQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+    {
+        id: 4,
+        title: "Cook With Comali Season 2",
+        thumbnailUrl: "https://www.youtube.com/embed/cPN8Pi06sEg",
+        likes: 32000,
+        views: 3200000,
+        channelName: "Vijay Television",
+        channelLogoUrl: "https://yt3.googleusercontent.com/O1B3gQKkFQlhsr0w6R5fhbNMGhlVHcIS6q6wsIX35C4xW9akkOxj1tATDLQYPKBO8oxgq7Dj=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+    {
+        id: 5,
+        title: "World’s Most Dangerous Escape Room!",
+        thumbnailUrl:"https://www.youtube.com/embed/3jS_yEK8qVI",
+        likes:5300000 
+        views: 236999000,
+        channelName: "MrBeast",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKY455xp16s2AIHalRjK60zas-DitxAHmRjQsXPE2A=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+
+    {
+        id: 6,
+        title: "LEO - Naa Ready Lyric Video",
+        thumbnailUrl:"https://www.youtube.com/embed/szvt1vD0Uug",
+        likes: 2700000,
+        views: 141000000,
+        channelName: "Sony Music South ",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKbGREEBS9tvkobo4vxoV9j-no0Hf9tSGx-vx6vp0Q=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+    {
+        id: 7,
+        title:"https://www.youtube.com/embed/aJC_2CfDVBk" ,
+        thumbnailUrl: "Day with Raveena darling ",
+        likes: 107000,
+        views: 1700000,
+        channelName: "Twin Throttlers",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKabZ-Jh_cVc8bmfuFXOoqP7MKdU5tyu635iTM-elw=s176-c-k-c0x00ffffff-no-rj-mo",
+    },
+
+    {
+        id: 8,
+        title: "Lockdown Kadhal",
+        thumbnailUrl:"https://www.youtube.com/embed/aWYxa83GETE",
+        likes: 304000,
+        views: 7000000 
+        channelName: "Eruma Saani",
+        channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKbQ0nw_huS2jQCKTzt7PtW9ovgnXDkoKK97guaAHg=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+    {
+        id: 9,
+        title: "Kalluri Saalai",
+        thumbnailUrl: "https://www.youtube.com/embed/qPIzbKrh2Xk" ,
+        likes:212000 ,
+        views: 8983000,
+        channelName: "Unakkennappa"
+        channelLogoUrl: "https://yt3.googleusercontent.com/QwXNtQejOQBlghkgBF6uKL-zMZFU1iHxeTQXSQ4UjbZzRtC4O-M08ILdSEo489AS9nSPmfgxGg=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+    {
+        id: 10,
+        title: "13 Arrears",
+        thumbnailUrl:"https://www.youtube.com/embed/9uT9rJh9tZw",
+        likes: 573000,
+        views: 14212000,
+        channelName: "Blacksheep",
+        channelLogoUrl: "https://yt3.googleusercontent.com/WJNHvrTF7J0meZ-TzBF-DRvptxwgajY_n7aSQYoDHIpmcyVsLzlVv0MsG_WycT5FDVuWt00wiw=s176-c-k-c0x00ffffff-no-rj",
+    },
+
+];
